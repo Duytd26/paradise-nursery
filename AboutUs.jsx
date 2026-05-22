2d6e8dfc489eb0de65a5b45acfa137ff354274ad
Duy Tran <trandinhduy26062006@gmail.com>
2026-05-22 09:41:21 +0700

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h1 className="about-us-description">Welcome to Paradise Nursery!</h1>
+      <p className="about-us-content">
+        Where Green Meets Serenity! At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and more inviting indoor lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every plant enthusiast.
+      </p>
+      <p className="about-us-content">
+        Our team of experts carefully selects and nurtures each plant, ensuring that they meet our strict standards for quality and health. Whether you are an experienced gardener or just starting your plant journey, we are here to support you every step of the way. Feel free to explore our collection and let the green vibes transform your space!
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
